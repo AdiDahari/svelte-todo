9f13ccd602ee2042e9ffafc9824f1479c32fdbec
Adi Dahari <dahariadi16@gmail.com>
2022-11-16 19:57:39 +0200

    html structure

---
+++ src/lib/todo-item.svelte
@@ -0,0 +1,15 @@
+<div class="">
+    <form action="" method="">
+        <input type="hidden" name="done" value="">
+        <button aria-label="mark done / not done">Done/NotDone</button>
+    </form>
+
+    <form action="" method="">
+        <input type="text">
+        <button>Save</button>
+    </form>
+
+    <form action="" method="">
+        <button aria-label="delete this todo">Delete</button>
+    </form>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,20 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    // @ts-nocheck
+    import TodoItem from "$lib/todo-item.svelte";
+    
+    const title = 'ToDos'
+</script>
+
+<svelte:head>
+    <title>{title}</title>
+</svelte:head>
+
+<h1>{title}</h1>
+
+<form action="" method="">
+    <input type="text" name="text" aria-label="Add a ToDo" placeholder="+ Tap to add a ToDd">
+</form>
+
+<TodoItem />
+<TodoItem />
+<TodoItem />
